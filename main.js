3cbcea8ddb58caa50b16aa298b1e0a7e005aa688
lukeschunk <lukeschunk@gmail.com>
2015-10-22 20:01:42 -0600

    trying to make this work...hi peter

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-    console.log("test");
-    var input = $("#main-input").val();
-    var input2 = $("#main-input");
+    
+    var input = $("#main-input");
 
 
     $("#main-input").on("focus", function () {
@@ -15,14 +14,16 @@ $(document).ready(function () {
 
     $("#main-input").on("keyup", function (event) {
         if (event.which == 13) {
-            addToList();
-            input2.val("");
+            addToList();  
         }
     });
 
     function addToList() {
         input = $("#main-input").val();
         $(".item-container").append(input);
+        $("#main-input").val("");
+        input.addClass("item"); //THIS IS THE LINE I NEED TO WORK
+                                //IT SHOULD LOOK LIKE THE OTHER P ELEMENTS                                 //THAT ARE HARD CODED IN THE HTML
         console.log(input);
 //        $(".item-container").append(input);
     }
