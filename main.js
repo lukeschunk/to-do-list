b698552bb495929bd2552c377d9d38493d51f868
lukeschunk <lukeschunk@gmail.com>
2015-10-30 22:44:31 -0600

    got animation working

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ $(document).ready(function () {
     var input = $("#main-input");
 
 
+    function changeInputSize(str) {
+        input = $('#main-input');
+        if (str.length > 20) {
+            input.css("font-size", "2.5em");
+            console.log('more than 20');
+        }
+    }
+
+    $("body").on("keyup", '#main-input', function() {
+        changeInputSize($("#main-input").val());
+//        console.log("this is working");
+    })
+
     $("#main-input").on("focus", function () {
         $(this).val("");
 
@@ -31,9 +44,18 @@ $(document).ready(function () {
             opacity: "0.3"
         }).addClass("secondClick");
     });
-    
-//    $(".secondClick").on('click', function(){
-//       $(this).remove; 
-//    });
 
+    //    $(".secondClick").on('click', function(){
+    //       $(this).remove; 
+    //    });
+
+    $(".item-container").on('click', '.secondClick', function () {
+        $(this).addClass("animation");
+        setTimeout(function (that) {
+            $(that).css("display", "none")
+        }, 1000, this)
+
+    });
 });
+
+//splice(. 1) when I click on this, splice this out of the array
